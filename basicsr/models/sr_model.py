9ddbe23cccfe9941fd720e6071b05d972f322e21
Zhaokh <55131043+zkh123456789@users.noreply.github.com>
2026-09-24 20:36:21 +0800

    Update sr_model.py
    Update image saving logic to use a different filename format.

--- basicsr/models/sr_model.py
+++ basicsr/models/sr_model.py
@@ -224,7 +224,7 @@ class SRModel(BaseModel):
 
                  else:
                         save_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
-                                                 f'{img_name}_{self.opt["name"]}.png')
+                                                 f'{img_name}.png')
                 imwrite(sr_img, save_img_path)
 
             if with_metrics:
